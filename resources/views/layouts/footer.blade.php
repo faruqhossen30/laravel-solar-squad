d26a13323cbd524f46b1d11c8d8916815f2e0df4
faruqhossen30 <faruqhossen30@gmail.com>
2024-08-06 16:54:19 +0600

    footer text change

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <section>
     <div class="lg:py-3 py-1 bg-blue-700">
-        <p class="text-center px-3 lg:text-2xl text-lg capitalize text-white">copyright {{date('Y')}}.all right reserved.</p>
+        <p class="text-center px-3 lg:text-2xl text-base capitalize text-white">© {{date('Y')}} Solar Squad. All Rights Reserved </p>
     </div>
 </section>
